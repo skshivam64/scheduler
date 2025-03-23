9040017e8b6dabd1d3d98d493f7287f4a53c3142
Shivam Kumar <skshivam64@gmail.com>
2025-03-23 18:06:13 +0530

    Fix imports in auto-generated protobuf modules

--- scheduler/proto/schedule_pb2_grpc.py
+++ scheduler/proto/schedule_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-import schedule_pb2 as schedule__pb2
+from . import schedule_pb2 as schedule__pb2
 
 GRPC_GENERATED_VERSION = '1.64.0'
 GRPC_VERSION = grpc.__version__
